b8b922a61ebefdb0de903a7b0b67d483291ff3a1
Emmanuel-EEzekiel <emamex44@gmail.com>
2022-02-17 21:49:16 +0100

    add file

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <section className='max-w-screen'>
-        <div className='bg-home-pattern bg-no-repeat bg-center bg-auto max-h-screen'>
-          <article className="h-full">
-            <p>We Change Everything Wordpress</p>
+      <section className='border-4 border-black-400 min-h-screen md:max-w-full mx-auto max-w-6x'>
+        <div className='bg-home-pattern bg-no-repeat bg-center  bg-cover bg-auto max-h-screen md:max-h-screen h-screen'>
+          <article className="h-full flex items-center flex-col justify-center ">
+            <p className="text-blue-300 text-4xl font-light">We Change Everything Wordpress</p>
             <p>This is the only wordpress theme you will ever want to use</p>
             <div>
               <button>Read More</button>
-              <button> Download Now</button>
+              <button>Download Now</button>
             </div>
           </article>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,18 +28,18 @@ const Navbar = () => {
   return (
     <div>
         <nav className="bg-white-100">
-            <div className='md:flex md:justify-between md:items-center'>
+            <div className='md:flex md:justify-between md:items-center md:px-6 md:max-w-6xl'>
                 <div className="flex justify-between items-center p-6" >
                     <p className="font-sans text-2xl text-black-200 font-light leading-loose"> Shaply Demo </p>
                     <button className="text-blue-800 md:hidden " onClick={toggleLinks}> <FaBars/> </button>
                 </div>
-                <div className='h-0 overflow-hidden transition duration-150 ease-linear pl-10 md:flex' ref={linksContainerRef}>
-                    <ul className='flex justify-evenly flex-col block transition duration-150 ease-linear' ref={linksRef}>
+                <div className='h-0 overflow-hidden transition duration-150 ease-linear pl-10 md:block md:h-auto' ref={linksContainerRef}>
+                    <ul className='md:flex md:' ref={linksRef}>
                         {links.map((link) => {
                         const { id, url, text } = link;
                         return (
-                            <li className="leading-10"key={id}>
-                            <NavLink className="text-black-200 text-sm font-medium" style={navLinkStyles} to={url}>{text}</NavLink>
+                            <li className="leading-10" key={id}>
+                            <NavLink className="text-black-200 text-sm font-medium transition duration-150 ease-linear mb-6 md:p-0 md:mx-1" to={url}>{text}</NavLink>
                             </li>
                         );
                         })}
