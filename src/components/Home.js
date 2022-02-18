278a6746c98cea4b400ab48d778d44efc90beaa5
Emmanuel-EEzekiel <emamex44@gmail.com>
2022-02-18 22:31:31 +0100

    add file

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,27 @@ const Home = () => {
         <div className='bg-home-pattern bg-no-repeat bg-center  bg-cover bg-auto max-h-screen md:max-h-screen h-screen'>
           <article className="h-full flex items-center flex-col justify-center ">
             <p className="text-white text-4xl font-light text-center mb-6 md:text-6xl">We Change Everything Wordpress</p>
-            <span classsName="text-red-500 text-sm leading-normal md:text-red-500">This is the only wordpress theme you will ever want to use</span>
+            <p classsName="text-slate-50 text-sm leading-normal md:text-red-500">This is the only wordpress theme you will ever want to use</p>
             <div className="flex flex-col md:flex md:flex-row">
               <button className="bg-purple-600 text-white-200 p-1 h-10 w-40 mt-6 text-white text-sm">Read More</button>
               <button className="border-2 border-white-400 p-1 h-10 w-40 mt-6 text-white text-sm md:ml-4">Download Now</button>
             </div>
           </article>
         </div>
+      </section>
+      <section className='min-h-screen md:max-w-full mx-auto max-w-6x'>
+        <div className='bg-white ma-h-screen h-screen'>
+          <article className=''>
+            <img src="" alt="" />
+            <div>
+              <h1></h1>
+              <p></p>
+              <button className='bg-purple-600 text-white-200 p-1 h-10 w-40 mt-6 text-white text-sm'> Read More</button>
+            </div>
+          </article>
+
+        </div>
+
       </section>
     </>
   )
